2f23a6a85d1e61c05a1ac011019b48346239cfe6
Harvey <harveyraybreeze@gmail.com>
2024-08-07 14:51:12 +0700

    Initial commit

---
+++ practice/cd_for_instance.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+import os
+
+# this appears the not be possible without a shell wrapper!
+
+# shell wrapper would have to execute the cd command in the current shell instance. 
+
+os.chdir("/")
